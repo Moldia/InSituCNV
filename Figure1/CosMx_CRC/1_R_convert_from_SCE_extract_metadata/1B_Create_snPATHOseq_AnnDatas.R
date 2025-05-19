630930a4cb42d1945afacdd60a067febcd5f80b4
augjensen <augusta.jensen@scilifelab.se>
2025-05-19 15:20:59 +0200

    update R scripts

--- Figure1/CosMx_CRC/1_R_convert_from_SCE_extract_metadata/1B_Create_snPATHOseq_AnnDatas.R
+++ Figure1/CosMx_CRC/1_R_convert_from_SCE_extract_metadata/1B_Create_snPATHOseq_AnnDatas.R
@@ -36,18 +36,20 @@ writeH5AD(
 #For the raw snPATHO-seq data
 
 raw_paths <- c(
-  "~/storage3/insituCNV/data/WTx-CosMx_TVA/round2/snPATHOseq/110-20250209T170643Z-001/110",
-  "~/storage3/insituCNV/data/WTx-CosMx_TVA/round2/snPATHOseq/120-20250209T170642Z-001/120",
-  "~/storage3/insituCNV/data/WTx-CosMx_TVA/round2/snPATHOseq/210-20250209T170645Z-001/210",
-  "~/storage3/insituCNV/data/WTx-CosMx_TVA/round2/snPATHOseq/221-20250209T170647Z-001/221",
-  "~/storage3/insituCNV/data/WTx-CosMx_TVA/round2/snPATHOseq/222-20250209T170637Z-001/222",
-  "~/storage3/insituCNV/data/WTx-CosMx_TVA/round2/snPATHOseq/230-20250209T170640Z-001/230",
-  "~/storage3//insituCNV/data/WTx-CosMx_TVA/round2/snPATHOseq/242-20250209T170638Z-001/242"
+  #"~/storage3/insituCNV/data/WTx-CosMx_TVA/round2/snPATHOseq/110-20250209T170643Z-001/110",
+  #"~/storage3/insituCNV/data/WTx-CosMx_TVA/round2/snPATHOseq/120-20250209T170642Z-001/120",
+  #"~/storage3/insituCNV/data/WTx-CosMx_TVA/round2/snPATHOseq/210-20250209T170645Z-001/210",
+  "~/storage3/insituCNV/data/WTx-CosMx_TVA/round2/snPATHOseq/221-20250209T170647Z-001/221"
+  #"~/storage3/insituCNV/data/WTx-CosMx_TVA/round2/snPATHOseq/222-20250209T170637Z-001/222",
+  #"~/storage3/insituCNV/data/WTx-CosMx_TVA/round2/snPATHOseq/230-20250209T170640Z-001/230",
+  #"~/storage3//insituCNV/data/WTx-CosMx_TVA/round2/snPATHOseq/242-20250209T170638Z-001/242"
 )
 
 for (path_obj in raw_paths){
   SID <- str_sub(path_obj, -3)
   snPATHOseq <- read10xCounts(path_obj)
+  snPATHOseq$Sample <- SID
+  
   writeH5AD(
     snPATHOseq,               # The SCE object
     file = paste0("/home/augusta/storage3/insituCNV/data/WTx-CosMx_TVA/round2/snPATHOseq/snPATHOseq_raw_",SID,".h5ad"),
